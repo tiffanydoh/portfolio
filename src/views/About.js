0786bf852e1a9ee5c49bc9f84cec154fd2669e19
Tiffany Do <tiffanydoh@gmail.com>
2022-05-15 22:52:19 -0700

    navbar and working on about me page

--- src/views/About.js
+++ src/views/About.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className='flex justify-around'>
+      <div>
+        <h1>About Me</h1>
+      </div>
+      <div >
+        <p>
+          <br />My name is Tiffany Do. I am currently a student at LEARN academy. LEARN Academy is a 4 month full stack web development bootcamp that prepares a diverse set of people for entry into the tech industry.
+          <br />
+          As the first in my family to go to college and being low income with a full ride, I really value education and prided myself in exploring different majors in hopes of finding what I was passionate in. After having graduated from UCSD in 2021, I was still very confused about my path and goals in life.
+          <br />
+          I quickly rediscovered my love for coding and the tech world when I recalled my years in First Lego League and Robotics club throughout middle and high school. First Lego League and Robotics inspired the youth to be hands-on with technology and to focus on design and coding. I was actually one of the few girls to join as it was mostly male dominated at school.
+          <br />
+          With rediscovering my passion in coding and tech, I want to transfer my acquired skills at LEARN academy to real world situations and further expand my knowledge and love for learning. I want to inspire and empower other girls to join the tech world and create a difference not only in their lives, but the world.
+        </p>
+      </div>
     </div>
+
   )
 }
 
