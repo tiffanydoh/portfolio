a7af901dd1d5c098911f3592baeb5ccc4207c6c2
Tiffany Do <tiffanydoh@gmail.com>
2022-05-25 17:15:17 -0700

    react scroll and working on home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+  const titles = ["full stack developer", "illustrator", "introvert", "!robot"];
 
-export default Home
+  return (
+    <div name="home" className="w-full h-screen bg-[#E9D3C0]">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <h1 className="font-bold text-[#F8F6F0] text-8xl">Tiffany Do</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
