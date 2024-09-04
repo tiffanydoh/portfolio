3e23afd41666e46051b2cd52246da8f36bfc7d34
Tiffany Do <tiffanydoh@gmail.com>
2024-09-04 15:39:05 -0700

    added three.js and created tab component

---
+++ src/component/WindowTab.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function WindowTab({ text }) {
+  return (
+    <div>
+      <div className="flex items-center px-4 py-2 border-2 border-black p-2 rounded-full text-sm font-bold ml-2">
+        <p>{text}</p>
+        <i className="pi pi-times text-sm ml-2" />
+      </div>
+    </div>
+  );
+}
--- src/component/WindowTemplate.tsx
+++ src/component/WindowTemplate.tsx
@@ -1,18 +1,23 @@
 import React from "react";
+import WindowTab from "./WindowTab.tsx";
 
 export default function WindowTemplate({ children }) {
   return (
-    <div className="flex items-center justify-center bg-white p-4">
+    <div className="flex items-center justify-center bg-white p-28">
       <div className="relative border-4 border-black max-w-5xl w-full rounded-lg shadow-xl shadow-gray-600">
         {/* Window Tab */}
         <div className="border-b-2 border-black flex items-center px-4 py-2 rounded-t-lg">
           <i className="pi pi-circle text-md mr-2" />
           <i className="pi pi-circle text-md mr-2" />
           <i className="pi pi-circle text-md mr-2" />
-          <div className="flex items-center px-4 py-2 border-2 border-black p-2 rounded-full text-sm font-bold ml-2">
-            <p>Welcome To My Portfolio</p>
-            <i className="pi pi-times text-sm ml-2" />
-          </div>
+
+          <WindowTab text="Welcome to Portfolio" />
+          {/* Experience */}
+          <WindowTab text="Experience" />
+          {/* Projects */}
+          <WindowTab text="Projects" />
+          {/* Recommendations */}
+          <WindowTab text="Recommendations" />
         </div>
 
         {/* Main Content */}
